3bcd12007fd114867a95e4619f625a984edc18a9
paulaami <pmilkowska@onet.pl>
2022-03-01 17:05:08 +0100

    updated files

---
+++ scripts/aos.js
@@ -0,0 +1,5 @@
+AOS.init({
+	startEvent: "DOMContentLoaded",
+	offset: 150,
+	once: true,
+});
---
+++ scripts/counter.js
@@ -0,0 +1,16 @@
+new PureCounter({
+  selector: '.purecounter',
+  start: 0, 
+  end: 100,
+  duration: 3,
+  delay: 10,
+  once: true,
+  repeat: false,
+  decimals: 0,
+  legacy: true,
+  filesizing: false,
+  currency: false,
+  separator: false,
+});
+
+new PureCounter();
